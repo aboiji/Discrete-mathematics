c9b420d725ffae51c19ec67ceda324181d8a931e
aboiji <wkdbdls9977@gmail.com>
2018-11-23 22:06:50 +0900

    집합과 행렬 연산 수정및 추가

--- 집합행렬연산.py
+++ 집합행렬연산.py
@@ -20,15 +20,25 @@ def sym_diff( s1, s2):
     return set1 ^ set2
 
 arr1 = numpy.array([[1,2],[3,4]])
-arr2 = numpy.asmatrix(arr1)
+matrix = numpy.asmatrix(arr1)
 
 
 def main():
+    print("집합 연산\n")
     print("set1 :", set1)
     print("set2 :", set2)
+    print()
 
     for f in [ union, intersection, complement, sym_diff]:
         print("{} {}".format( f.__name__, f( set1, set2)))
+    
+    print("\n\n 행렬 연산\n")
+    print("배열\n", arr1)
+    print("행렬\n", matrix)
+    print("행렬 곱 \n{}\n".format( matrix*matrix))
+    print("전치 행렬\n{}".format( matrix.T))
+
+
 
 if __name__ == "__main__":
     main()
